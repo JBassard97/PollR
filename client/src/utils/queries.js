805ffd5d26c3e57787661bde8f8d5c5d186ad107
Jbassard <jonathanacciarito@gmail.com>
2024-02-29 20:36:20 -0500

    added all of the queries and mutations we need in frontend

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,167 @@
 import { gql } from "@apollo/client";
 
 // TODO: ADD HERE FOR THE QUERIES
-// me
-// users
-// user
-// polls
-// poll
+// queries.js
+
+import { gql } from "@apollo/client";
+
+export const GET_CURRENT_USER = gql`
+  query Me {
+    me {
+      _id
+      username
+      email
+      pollCount
+      voteCount
+      pollCount
+      pollsMade {
+        _id
+        header
+        description
+        voteCount
+        votes {
+          choice {
+            voteCount
+          }
+        }
+        choices {
+          text
+          voteCount
+          votes {
+            choice {
+              text
+              voteCount
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_ALL_USERS = gql`
+  query Users {
+    users {
+      _id
+      email
+      username
+      voteCount
+      pollCount
+      pollsMade {
+        _id
+        header
+        description
+        voteCount
+        votes {
+          choice {
+            voteCount
+          }
+        }
+        choices {
+          text
+          voteCount
+          votes {
+            choice {
+              text
+              voteCount
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_USER_BY_USERNAME = gql`
+  query GetUserByUsername($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      pollCount
+      voteCount
+      pollCount
+      pollsMade {
+        _id
+        header
+        description
+        voteCount
+        votes {
+          choice {
+            voteCount
+          }
+        }
+        choices {
+          text
+          voteCount
+          votes {
+            choice {
+              text
+              voteCount
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_ALL_POLLS = gql`
+  query Polls {
+    polls {
+      _id
+      header
+      description
+      choices {
+        _id
+        text
+      }
+      creator {
+        _id
+        username
+      }
+      votes {
+        _id
+        user {
+          _id
+          username
+        }
+        choice {
+          _id
+          text
+        }
+      }
+      voteCount
+    }
+  }
+`;
+
+export const GET_POLL_BY_ID = gql`
+  query GetPollById($_id: ID!) {
+    poll(_id: $_id) {
+      _id
+      header
+      description
+      choices {
+        _id
+        text
+      }
+      creator {
+        _id
+        username
+      }
+      votes {
+        _id
+        user {
+          _id
+          username
+        }
+        choice {
+          _id
+          text
+        }
+      }
+      voteCount
+    }
+  }
+`;
