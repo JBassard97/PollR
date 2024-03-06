24c126cc6671994e39640530deb58e8c5f66d484
Jbassard <jonathanacciarito@gmail.com>
2024-03-06 16:45:28 -0500

    added error messages on poll when user aint logged in or voted twice

--- client/src/components/Poll/index.jsx
+++ client/src/components/Poll/index.jsx
@@ -1,19 +1,18 @@
 import "./poll.css";
-import React from "react";
+import {useState} from "react";
 import { useMutation } from "@apollo/client";
 import { CREATE_VOTE } from "../../utils/mutations";
 import Auth from "../../utils/auth";
 
 const Poll = ({ poll }) => {
   const [createVote, { error, data }] = useMutation(CREATE_VOTE);
+  const [errorMessage, setErrorMessage] = useState(false);
 
-  let hasUserVoted = false; // Initialize outside of the conditional block
+  let hasUserVoted = false;
   if (Auth.loggedIn()) {
     const currentUserId = Auth.getProfile().authenticatedPerson._id || null;
     hasUserVoted = poll.votes.some((vote) => vote.user._id === currentUserId);
-    console.log(hasUserVoted, poll.header);
   }
-  console.log(poll);
 
   const handleChoiceClick = async (choiceId) => {
     if (!poll || !poll._id) {
@@ -21,14 +20,28 @@ const Poll = ({ poll }) => {
       return;
     }
 
-    console.log("Choice Id:", choiceId);
-    console.log("Poll Id:", poll._id);
-    try {
-      const { data } = await createVote({
-        variables: { pollId: poll._id, choiceId: choiceId },
-      });
-    } catch (error) {
-      console.error("error:", error);
+    if (hasUserVoted || !Auth.loggedIn()) {
+      handleInvalidVote(choiceId); // Call handleInvalidVote with choiceId
+    } else {
+      console.log("Choice Id:", choiceId);
+      console.log("Poll Id:", poll._id);
+      try {
+        const { data } = await createVote({
+          variables: { pollId: poll._id, choiceId: choiceId },
+        });
+      } catch (error) {
+        console.error("error:", error);
+      }
+    }
+  };
+
+  const handleInvalidVote = (choiceId) => {
+    if (!Auth.loggedIn()) {
+      console.log("Log In/Sign Up to Vote")
+      setErrorMessage(<p className="error">Log In to Vote!</p>)
+    } else {
+      console.log("You can't vote twice!", choiceId); // Log choiceId
+      setErrorMessage(<p className="error">Already Voted!</p>)
     }
   };
 
@@ -38,12 +51,13 @@ const Poll = ({ poll }) => {
         <p className="pollHeader">{poll.header}</p>
         <p className="pollDesc">{poll.description}</p>
       </div>
+      {!errorMessage ? (<></>):(errorMessage)}
       {poll.choices.map((choice, index) => (
         <p
-          className={`pollChoice ${hasUserVoted ? "disabled" : ""}`} // Apply disabled class conditionally
+          className={`pollChoice ${hasUserVoted ? "disabled" : ""}`}
           id={choice._id}
           key={index}
-          onClick={hasUserVoted ? null : () => handleChoiceClick(choice._id)}
+          onClick={() => handleChoiceClick(choice._id)} // Pass choiceId to handleChoiceClick
         >
           {choice.text}
         </p>
@@ -54,62 +68,3 @@ const Poll = ({ poll }) => {
 };
 
 export default Poll;
-
-// import "./poll.css";
-// import React from "react";
-// import { useMutation } from "@apollo/client";
-// import { useState } from "react";
-// import { CREATE_VOTE } from "../../utils/mutations";
-// import Auth from "../../utils/auth";
-
-// const Poll = ({ poll }) => {
-//   const [createVote, { error, data }] = useMutation(CREATE_VOTE);
-
-//   if (Auth.loggedIn()) {
-//     const currentUserId = Auth.getProfile().authenticatedPerson._id || null;
-
-//     const hasUserVoted = poll.votes.some(
-//       (vote) => vote.user._id === currentUserId
-//     );
-//   }
-//   console.log(poll);
-
-//   const handleChoiceClick = async (choiceId) => {
-//     if (!poll || !poll._id) {
-//       console.error("Poll or poll ID is undefined");
-//       return;
-//     }
-
-//     console.log("Choice Id:", choiceId);
-//     console.log("Poll Id:", poll._id);
-//     try {
-//       const { data } = await createVote({
-//         variables: { pollId: poll._id, choiceId: choiceId },
-//       });
-//     } catch (error) {
-//       console.error("error:", error);
-//     }
-//   };
-
-//   return (
-//     <div className="poll" id={poll._id}>
-//       <div>
-//         <p className="pollHeader">{poll.header}</p>
-//         <p className="pollDesc">{poll.description}</p>
-//       </div>
-//       {poll.choices.map((choice, index) => (
-//         <p
-//           className="pollChoice"
-//           id={choice._id}
-//           key={index}
-//           onClick={hasUserVoted ? null : () => handleChoiceClick(choice._id)}
-//         >
-//           {choice.text}
-//         </p>
-//       ))}
-//       <p className="createdBy">Created by: {poll.creator.username}</p>
-//     </div>
-//   );
-// };
-
-// export default Poll;
