21e6599cf9e772e4d019f660b7cc82505e83fa16
Anthony <anthony.l.michel@gmail.com>
2024-03-07 21:51:32 -0500

    testing modal and graphs

--- client/src/components/Poll/Graph.jsx
+++ client/src/components/Poll/Graph.jsx
@@ -5,30 +5,50 @@ import '../../App.css';
 import '../../../../node_modules/react-vis/dist/style.css'
 
 
-const Graph = ({poll}) => {
+// const Graph = ({poll}) => {
 
-    const pollData = poll.data.map((data) => {
-        return {x: data.choices.text,
-        y: data.voteCount}
-    })
+//     const pollData = poll.data.map((data) => {
+//         return {x: data.choices.text,
+//         y: data.voteCount}
+//     })
 
-    if (loading) {
-        return <div>Loading...</div>
-    }
+//     if (loading) {
+//         return <div>Loading...</div>
+//     }
     
+//     return (
+//         <div>
+//             <XYPlot 
+//                 width={300} 
+//                 height={250}>
+//             <XAxis/>
+//             <YAxis title="Number of votes"/>
+//             <VerticalBarSeries 
+//                 data={pollData} 
+//                 style={{ color: 'red'}}/>
+//             </XYPlot>  
+//         </div>
+//     );
+// };
+
+const Graph = () => {
     return (
-        <div>
-            <XYPlot 
-                width={300} 
-                height={250}>
-            <XAxis/>
-            <YAxis title="Number of votes"/>
-            <VerticalBarSeries 
-                data={pollData} 
-                style={{ color: 'red'}}/>
-            </XYPlot>  
-        </div>
-    );
+                <div>
+                    <XYPlot 
+                        width={350} 
+                        height={350}>
+                    <XAxis/>
+                    <YAxis title="Number of votes"/>
+                    <VerticalBarSeries 
+                        data={[
+                            {x: 1, y: 4},
+                            {x: 5, y: 2},
+                            {x: 15, y: 6}
+                        ]}
+                        style={{ color: 'red'}}/>
+                    </XYPlot>  
+                </div>
+            );
 };
 
 export default Graph;
